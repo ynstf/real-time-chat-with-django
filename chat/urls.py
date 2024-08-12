b315d6da7ae1350262f68b36828955f1382247a6
YONES <107154559+ynstf@users.noreply.github.com>
2024-08-12 16:06:19 +0100

    fix urls

--- chat/urls.py
+++ chat/urls.py
@@ -3,8 +3,5 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('<str:room_name>/', views.room, name='room'),
 ]
